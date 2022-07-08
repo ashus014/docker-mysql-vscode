d6b8c02bfd37773ddf69bb8124d43b564d90bb6b
Ashu <ashu.ask.singh@gmail.com>
2022-07-08 14:17:27 +0530

    working with sql database

---
+++ 01-create-database.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE talently;
+
+
+/* 
+SQL mainly consists following data types : Text | Numeric | Date | Other
+
+---Let's see Text---
+CHAR(x) -> Text upto x characters long
+VARCHAR(x) -> you have option to insert upto 'x' many characters
+ENUM -> only for few selected values 
+
+---Let's see Numberic---
+INT ->
+DECIMAL, NUMERIC ->
+FLOAT, REAL ->
+
+---Let's see Date
+DATE ->
+DATETIME, TIMESTAMP ->
+
+---Other
+BOOLEAN -> 
+JSON ->
+*/
+
