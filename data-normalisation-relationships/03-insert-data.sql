70743248b95e53889011339bc4227d45cc24781e
Ashu Singh <ashu.ask.singh@gmail.com>
2022-11-19 00:20:25 +0530

    working with normalisation

---
+++ data-normalisation-relationships/03-insert-data.sql
@@ -0,0 +1,9 @@
+-- INSERT INTO cities (name)
+-- VALUES ('Berlin'),('New York'),('London');
+
+INSERT INTO addresses (street, house_number, city_id)
+VALUES 
+('Teststreet', '8A',3),
+('Some street', '10',1),
+('Teststreet', '1',3),
+('My street', '101',2);
